f32d188c8547753b54e75ec56d3bfedffdb2c4bf
Frank <1850774695@qq.com>
2015-11-18 22:45:39 +0800

    output several words

--- trie.py
+++ trie.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import re
 from datetime import datetime
 
@@ -7,39 +8,65 @@ class Node:
     def __init__(self):
         self.value = None
         self.children = {}    # children is of type {char, Node}                                                                                                       
- 
+
 class Trie:
     def __init__(self):
         self.root = Node()
-        self.get_words = {}
-        self.words = {}
+        self.get_words = {} 
+        self.words_weight = {}
         
- 
-    def insert(self, key):      # key is of type string                                                                                                                
-        # key should be a low-case string, this must be checked here!       
-        key = key.lower()                                                                                           
+    def insert(self, key):      # key is of type string                         
+        key = key.lower()      # key should be a low-case string, this must be check                                                                                  
         node = self.root
         for char in key:
             if char not in node.children:
                 child = Node()
                 node.children[char] = child
                 node = child
-                node.char = char
             else:
                 node = node.children[char]
         node.value = key
- 
-    def search(self, key):
-        node = self.root
-        # error percentage
-        if len(key)<=6:
+
+    def get_error_length(self,key):
+        if len(key)<=6: # error percentage
             rate = 0.2
         elif len(key)>=18:
             rate = 0.2
         else:
             rate = 0.25
 
-        num = int(round( len(key)*rate ))  # allow error num
+        return int(round( len(key)*rate ))
+
+    def best_match(self):
+        best_match_words = {}
+        best_word_weight = []
+
+        for word in self.get_words: # get words with weight
+            if word in self.words_weight:
+                if self.get_words[word] < self.words_weight[word]:
+                    self.get_words[word] = self.words_weight[word]
+
+        max_weight = 0 # get max_weight word
+        for word,value in self.get_words.iteritems():
+            if max_weight < value:
+                max_weight = value
+
+        for word,value in self.get_words.iteritems(): # get max_weight nearby words
+            if (max_weight/value) < 2:
+                best_match_words[word] = value
+                best_word_weight.append(value)
+        
+        best_word_weight = sorted(best_word_weight,reverse = True)
+
+        for weight in best_word_weight:
+            for word,value in best_match_words.iteritems():
+                if weight == value:
+                    print word
+ 
+    def search(self, key):
+        node = self.root     
+        num = self.get_error_length(key)   # allow error num
+
         for char in key:
             if char in node.children:
                 node = node.children[char]
@@ -48,59 +75,43 @@ class Trie:
             # entire right
             print node.value
         else:
-            # some error
-            self.dfs(key,self.root,num)
+            # No found on trie 
+            self.dfs(key,self.root,num,0)
 
-            for word in self.get_words:
-                if word in self.words:
-                    if self.get_words[word] < self.words[word]:
-                        self.get_words[word] = self.words[word]
+            self.best_match()
 
-            # self.get_words= sorted(self.get_words.iteritems(), key=lambda d:d[1], reverse = True)
-            print self.get_words        
-
-    def dfs(self,key,node,num):
+    def dfs(self,key,node,num,match):
         keep_num = num
         i = 0  
         if num > 0:
-            # blank char
-            for node_char in node.children:
-                self.dfs(key,node.children[node_char],num-1)
+            for node_char in node.children: # blank char
+                self.dfs(key,node.children[node_char],num-1,match)
 
-            # more char
-            self.dfs(key[1:],node,num-1)
+            self.dfs(key[1:],node,num-1,match) # more char
 
         for char in key:
             i +=1
-            # error char
-            if num > 0:
+            if num > 0: # error char
                 for error_char in node.children:
-                    self.dfs(key[i:],node.children[error_char],num-1)
+                    self.dfs(key[i:],node.children[error_char],num-1,match)
                 num -=1
-            # right char
-            if char in node.children:
+            
+            if char in node.children: # right char
                 node = node.children[char]
-                self.dfs(key[i:],node,keep_num)
+                match +=1
+                self.dfs(key[i:],node,keep_num,match)
             else:
                 if num <= 0:
                     return
-        # record word
-        if node.value and (not key):
+        
+        if node.value and (not key): # record word
             if node.value in self.get_words:
-                self.get_words[node.value] +=1
+                if match > self.get_words[node.value]:
+                    self.get_words[node.value] = match
             else:
-                self.get_words[node.value] = 1
+                self.get_words[node.value] = match
 
-    # def display_node(self, node):
-    #     if node.value:
-    #         print node.value
-    #     for char in node.children:
-    #             self.display_node(node.children[char])
- 
-    # def display(self):
-    #     self.display_node(self.root)
-
-    def word_weight(self):
+    def get_word_weight(self):
         filename = "frequent_words.csv"
         f = open(filename)
         lines = f.readlines()
@@ -110,9 +121,9 @@ class Trie:
             line = line.split(',')
             word = line[0].lower()
             weight = line[1]
-            self.words[word] = int(weight)
+            self.words_weight[word] = int(weight)
 
-    def get_trie(self):
+    def create_trie(self):
         filename = "words.txt"
         f = open(filename)
         lines = f.readlines()
@@ -120,11 +131,12 @@ class Trie:
         for line in lines:
             self.insert(re.sub(r'\n','',line))
 
-    def start(self):
-        self.get_trie()
-        self.word_weight()
+    def main(self):
+        self.create_trie()
+        self.get_word_weight()
         n = int(raw_input('please input number : '))
-        for x in xrange(0,n):
+
+        for x in range(0,n):
             key = raw_input('please input word : ')
             key = key.lower()
             if len(key) > 22 or re.search(r'[^a-z]',key):
@@ -135,13 +147,8 @@ class Trie:
                 # print datetime.now()
                 self.get_words = {}
                 
-
-
-
 if __name__=='__main__':
  
     trie = Trie()
 
-    trie.start()
-# trie.display()
-# trie.search('ealous')
+    trie.main()
